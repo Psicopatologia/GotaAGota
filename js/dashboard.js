917234d61995e12e13758d568eed0b01a8259668
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2020-09-29 22:21:44 -0500

    Añadido la pantalla de menú

---
+++ js/dashboard.js
@@ -0,0 +1,53 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Domingo',
+        'Lunes',
+        'Martes',
+        'Miercoles',
+        'Jueves',
+        'Viernes',
+        'Sábado'
+      ],
+      datasets: [{
+        data: [
+          10,
+          30,
+          40,
+          20,
+          30,
+          35,
+          35
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#FF525D',
+        borderWidth: 4,
+        pointBackgroundColor: '#FF525D'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+}())
