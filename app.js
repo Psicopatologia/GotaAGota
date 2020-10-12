9cfefb7e760181e4ca712685829afef82fd6d4da
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2020-10-12 11:31:28 -0500

    Se añadió conección a base de datos

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// initializations 
 var app = express();
+require('./database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
